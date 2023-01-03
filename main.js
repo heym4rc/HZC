cf20cdd7fba5be2df2a59b959a4d00b54355d5b8
heym4rc <marcioreg77@gmail.com>
2023-01-03 12:01:08 -0300

    Navbar (fechar ao clilcar) e Button - backToTop

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ const nav = document.querySelector('.navbar')
 buttonMenu.addEventListener('click', () => {
         nav.classList.toggle('navbar-active')
 })
+
+// CLOSE MENU - CLICK ITEM
+const links = document.querySelectorAll('nav a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('navbar-active')
+    })
+}
+
+/* BUTTON BACK TO TOP */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* WHEN SCROLL */
+window.addEventListener('scroll', function () {
+        backToTop()
+})
